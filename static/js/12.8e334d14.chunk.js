0bc7d04dee8b0c9ac4362d433db5cd0ba75b574d
Prajwal Gupta <prajwal@Prajwals-MacBook-Air.local>
2023-12-22 18:30:50 +0530

    publishing to npm.

---
+++ static/js/12.8e334d14.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwhatmorelive_react=self.webpackChunkwhatmorelive_react||[]).push([[12],{9723:function(e,a,r){var t=r(5318);a.Z=void 0;var c=t(r(5045)),l=r(6417),u=(0,c.default)((0,l.jsx)("path",{d:"M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18c.62-.39.62-1.29 0-1.69L9.54 5.98C8.87 5.55 8 6.03 8 6.82z"}),"PlayArrowRounded");a.Z=u}}]);
